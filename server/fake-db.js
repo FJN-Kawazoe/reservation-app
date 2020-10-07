583e1cce7dc22634ae4224465df2ec8a037acf7a
FJN-Kawazoe <k.kawazoe@fujinetworks.co.jp>
2020-10-07 15:17:15 +0900

    Nodeサーバー構築

---
+++ server/fake-db.js
@@ -0,0 +1,84 @@
+const Product = require('./model/product')
+
+class FakceDb {
+
+  constructor() {
+    this.products = [
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Pro',
+        price: 899,
+        description: 'It has all the necessary functions',
+        heading1: 'Sample text1',
+        heading2: 'Sample text2',
+        heading3: 'Sample text3',
+        headingtext1: 'Sample Sample Sample Sample Sample Sample Sample Sample Sample',
+        headingtext2: 'SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE',
+        headingtext3: 'サンプル サンプル サンプル サンプル サンプル サンプル サンプル サンプル',
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heading1: 'Sample text1',
+        heading2: 'Sample text2',
+        heading3: 'Sample text3',
+        headingtext1: 'Sample Sample Sample Sample Sample Sample Sample Sample Sample',
+        headingtext2: 'SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE',
+        headingtext3: 'サンプル サンプル サンプル サンプル サンプル サンプル サンプル サンプル',
+    
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras',
+        heading1: 'Sample text1',
+        heading2: 'Sample text2',
+        heading3: 'Sample text3',
+        headingtext1: 'Sample Sample Sample Sample Sample Sample Sample Sample Sample',
+        headingtext2: 'SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE',
+        headingtext3: 'サンプル サンプル サンプル サンプル サンプル サンプル サンプル サンプル',
+    
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: '',
+        heading1: 'Sample text1',
+        heading2: 'Sample text2',
+        heading3: 'Sample text3',
+        headingtext1: 'Sample Sample Sample Sample Sample Sample Sample Sample Sample',
+        headingtext2: 'SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE SAMPLE',
+        headingtext3: 'サンプル サンプル サンプル サンプル サンプル サンプル サンプル サンプル',
+    
+      }
+    ]
+  }
+
+  async initDb() {
+    await this.cleanDb()
+    this.pushProductsToDb()
+  }
+
+  async cleanDb() {
+    await Product.deleteMany({})
+  }
+
+  pushProductsToDb() {
+    this.products.forEach(
+      (product) => {
+        const newProduct = new Product(product)
+        newProduct.save()
+      }
+    )
+  }
+
+  seeDb() {
+    this.pushProductsToDb()
+  }
+}
+
+module.exports = FakceDb
